88ce6fcae81968d76e6cf2a2b285842ef7e31646
Afeez <aoladokun@my.apsu.edu>
2023-03-19 16:13:32 -0400

    Adding test class for each controller service

--- src/test/java/com/ltp/contacts/ContactsApplicationTests.java
+++ src/test/java/com/ltp/contacts/ContactsApplicationTests.java
@@ -1,13 +1,106 @@
 package com.ltp.contacts;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ltp.contacts.pojo.Contact;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import com.ltp.contacts.repository.ContactRepository;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ContactsApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@Autowired
+	private ContactRepository contactRepository;
 
 	@Test
 	void contextLoads() {
 	}
+
+	private Contact[] contacts = new Contact[] {
+			new Contact("1", "Jon Snow", "6135342524"),
+			new Contact("2", "Tyrion Lannister", "4145433332"),
+			new Contact("3", "The Hound", "3452125631"),
+	};
+
+	@BeforeEach
+	void setup(){
+		for (int i = 0; i < contacts.length; i++) {
+			contactRepository.saveContact(contacts[i]);
+		}
+	}
+
+	@AfterEach
+	void clear(){
+		contactRepository.getContacts().clear();
+	}
+
+	@Test
+	public void getContactByIdTest() throws Exception {
+		RequestBuilder request = MockMvcRequestBuilders.get("/contact/1");
+		mockMvc.perform(request)
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$.name").value(contacts[0].getName()))
+				.andExpect(jsonPath("$.phoneNumber").value(contacts[0].getPhoneNumber()));
+	}
+
+	@Test
+	public void getAllContactsTest() throws Exception {
+		RequestBuilder request = MockMvcRequestBuilders.get("/contact/all");
+		mockMvc.perform(request)
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$.size()").value(3))
+				.andExpect(jsonPath("$.[?(@.phoneNumber == \"6135342524\" && @.name == \"Jon Snow\")]").exists());
+	}
+
+	@Test
+	public void validContactCreation() throws Exception {
+		RequestBuilder request = MockMvcRequestBuilders.post("/contact")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(new Contact(null, "Jon", "9312661677")));
+		mockMvc.perform(request)
+				.andExpect(status().isCreated());
+	}
+
+	@Test
+	public void invalidContactCreation() throws Exception {
+		RequestBuilder request = MockMvcRequestBuilders.post("/contact")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(new Contact(null, "        ", "     ")));
+		mockMvc.perform(request)
+				.andExpect(status().isBadRequest());
+	}
+
+	@Test
+	public void contactNotFoundTest() throws Exception {
+		RequestBuilder request = MockMvcRequestBuilders.get("/contact/4");
+		mockMvc.perform(request)
+				.andExpect(status().isNotFound());
+
+	}
+
+
+
+
+
+	
+	
 
 }
